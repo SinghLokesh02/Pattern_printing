e92cbf610edcda4a2f1792f1352e3aa9c2f5fedc
Lokesh <lokeshsingh7695@gmail.com>
2024-07-31 08:58:25 +0530

    Pattern 29

--- pattern_29.cpp
+++ pattern_29.cpp
@@ -2,10 +2,31 @@
 using namespace std;
 int main()
 {
+    string s = "aeiou";
     int n;
     cout << "Enter the number of rows: ";
     cin >> n;
-    
-    
+    int count = 0;
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<3;j++){
+            if(count == 5)count = 0;
+            cout<<1<<" "<<s[count]<<" ";
+            count++;
+        }
+        cout<<endl;
+    }    
     return 0;
 }
+
+
+/*
+Output
+
+Enter the number of rows: 4
+
+1 a 1 e 1 i
+1 o 1 u 1 a
+1 e 1 i 1 o
+1 u 1 a 1 e
+
+*/
