99ae557b8b8d94d37b0210e205df26867ceecf1b
Lokesh <lokeshsingh7695@gmail.com>
2024-08-06 09:46:15 +0530

    Pattern 37

---
+++ Pattern_37.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the number of rows : ";
+    cin >> n;
+    // Upper
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int star = 0; star < 2 * i + 1; star++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    // Lower
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << "  ";
+        }
+        for (int star = 0; star < 2 * i + 1; star++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
+/*
+
+Enter the number of rows : 5
+
+          *
+        * * *
+      * * * * *
+    * * * * * * *
+  * * * * * * * * *
+* * * * * * * * * * *
+  * * * * * * * * *
+    * * * * * * *
+      * * * * *
+        * * * 
+          *
+
+
+
+*/
