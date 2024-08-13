3b020a8e4b1961a619ddc6ae765dc7a1ced8ee61
Lokesh <lokeshsingh7695@gmail.com>
2024-08-13 06:57:10 +0530

    Pascal Triangle using Array

--- Pattern_41.cpp
+++ Pattern_41.cpp
@@ -1,35 +1,90 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+// // Using Vector
 
-vector<vector<int>> PascalTriangle(int n)
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// vector<vector<int>> PascalTriangle(int n)
+// {
+//     vector<vector<int>> ans(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         ans[i].resize(i + 1);
+//         ans[i][0] = ans[i][i] = 1;
+//         for (int j = 1; j < i; j++)
+//         {
+//             ans[i][j] = (ans[i - 1][j] + ans[i - 1][j - 1]);
+//         }
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     int n;
+//     cout << "Enter the number of rows : ";
+//     cin >> n;
+//     vector<vector<int>> ans = PascalTriangle(n);
+//     for (auto i : ans)
+//     {
+//         for (int s = 0; s < n - i.size(); s++)
+//             cout << " ";
+//         for (auto j : i)
+//             cout << j << " ";
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
+
+
+// Using Array
+
+#include <iostream>
+ 
+using namespace std;
+// Using Vector
+void PascalTriangle(int n)
 {
-    vector<vector<int>> ans(n);
-    for (int i = 0; i < n; i++)
-    {
-        ans[i].resize(i + 1);
-        ans[i][0] = ans[i][i] = 1;
-        for (int j = 1; j < i; j++)
-        {
-            ans[i][j] = (ans[i - 1][j] + ans[i - 1][j - 1]);
+    // Initially all the Value will be initialized to zero
+    int arr[n][n];
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n;j++){
+            arr[i][j] = 0;
         }
     }
-    return ans;
+
+    // Creating the Pascal Triangle
+    for (int i = 0; i < n; i++)
+    {
+        arr[i][0] = arr[i][i] = 1;
+        for (int j = 1; j < i; j++)
+        {
+            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+        }
+         
+    }
+    
+    // Printing the Pascal Triangle
+    for (int i = 0; i < n; i++)
+    {
+        // Spaces
+        for (int j = 0; j < n - i - 1; j++)
+            cout << " ";
+
+        // Values
+        for (int j = 0; j <= i; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 int main()
 {
     int n;
     cout << "Enter the number of rows : ";
     cin >> n;
-    vector<vector<int>> ans = PascalTriangle(n);
-    for (auto i : ans)
-    {
-        for (int s = 0; s < n - i.size(); s++)
-            cout << " ";
-        for (auto j : i)
-            cout << j << " ";
-        cout << endl;
-    }
+    PascalTriangle(n);
+     
 
     return 0;
 }
